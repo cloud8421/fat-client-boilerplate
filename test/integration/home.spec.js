6f37108ebf38d7f6d5496ae39e7ae95c865d9cc5
Claudio Ortolina <claudio@jumpzero.com>
2013-02-11 17:56:29 +0000

    Extra specs and jshint fixes.

--- test/integration/home.spec.js
+++ test/integration/home.spec.js
@@ -2,21 +2,27 @@ var jasmine = require('jasmine-node');
 var zombie = require('zombie');
 var browser = new zombie.Browser();
 
-function whenPageHasLoaded(callback) {
-  browser.visit('http://localhost:8000/', function(error, browser) {
+function visit(path, callback) {
+  browser.visit('http://localhost:8001' + path, function (error, browser) {
     callback.call();
-  })
+  });
   jasmine.asyncSpecWait();
 }
 
-describe('home page', function() {
+describe('home page', function () {
 
-  describe('initial', function() {
-    it('has correct title', function() {
-      whenPageHasLoaded(function() {
+  describe('initial', function () {
+    it('has correct title', function () {
+      visit('/', function () {
         expect(browser.text('title')).toEqual('Fat client boilerplate');
         jasmine.asyncSpecDone();
-      })
+      });
+    });
+    it('has the right body', function () {
+      visit('/', function () {
+        expect(browser.text('h1')).toEqual('It works!');
+        jasmine.asyncSpecDone();
+      });
     });
   });
 
