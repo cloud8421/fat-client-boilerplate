c8b279733e72de87e2ca865c171ab50a017176ef
Claudio Ortolina <claudio@jumpzero.com>
2013-02-17 08:31:03 +0000

    Updates for Grunt 0.4.0
    Signed-off-by: Claudio Ortolina <claudio@jumpzero.com>

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,19 @@
+var path = require('path');
+var lrSnippet = require('grunt-contrib-livereload/lib/utils').livereloadSnippet;
+
+var folderMount = function folderMount(connect, point) {
+  return connect.static(path.resolve(point));
+};
+
 module.exports = function (grunt) {
   'use strict';
 
-  grunt.loadNpmTasks('grunt-reload');
-  grunt.loadNpmTasks('grunt-requirejs');
+  grunt.loadNpmTasks('grunt-contrib-livereload');
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-hash');
+  grunt.loadNpmTasks('grunt-regarde');
   grunt.loadNpmTasks('grunt-shell');
   grunt.loadTasks('tasks');
 
@@ -24,18 +34,13 @@ module.exports = function (grunt) {
         manifest: 'dist/assets.json'
       }
     },
-    lint: {
+    jshint: {
       grunt: ['grunt.js'],
       src:   ['src/{collections,models,regions,views}/**/*.js', 'src/*.js'],
-      test:  ['test/**/*.js']
-    },
-    jshint: {
-      options: {indent: 2},
-      src: {
-        globals: {define: true, require: true}
-      },
-      test: {
-        globals: {describe: true, require: true, it: true, jasmine: true, reporter: true}
+      test:  ['test/**/*.js'],
+      options: {
+        indent: 2,
+        globals: {describe: true, define: true, require: true, it: true, jasmine: true, reporter: true}
       }
     },
     requirejs: {
@@ -75,19 +80,27 @@ module.exports = function (grunt) {
       },
       jasmine: {
         command: 'phantomjs test/support/jasmine-runner.coffee http://localhost:8001/headlessRunner.html',
-        stdout: true
+        options: {
+          stdout: true
+        }
       },
       zombie: {
         command: '`npm bin`/jasmine-node test/integration',
-        stdout: true
+        options: {
+          stdout: true
+        }
       },
       'sass-development': {
         command: 'sass -l -t expanded stylesheets/main.scss:build/main.css',
-        stdout: true
+        options: {
+          stdout: true
+        }
       },
       'sass-production': {
         command: 'sass -t compressed stylesheets/main.scss:build/main.min.css',
-        stdout: true
+        options: {
+          stdout: true
+        }
       }
     },
     hash: {
@@ -95,54 +108,62 @@ module.exports = function (grunt) {
       mapping: 'dist/assets.json',
       dest: 'dist/'
     },
-    watch: {
+    regarde: {
       grunt: {
         files: ['grunt.js'],
-        tasks: 'lint:grunt'
+        tasks: ['jshint:grunt'],
+        spawn: true
       },
       src: {
         files: ['src/*.js',
                 'src/{collections,models,regions,views}/**/*.js'
         ],
-        tasks: 'lint:src lint:test reload shell:jasmine shell:zombie'
+        tasks: ['jshint:src', 'jshint:test', 'livereload', 'shell:jasmine', 'shell:zombie']
       },
       test: {
         files: ['test/**/*.js'],
-        tasks: 'lint:test shell:jasmine shell:zombie'
+        tasks: ['jshint:test', 'shell:jasmine', 'shell:zombie'],
+        spawn: true
       },
       stylesheet: {
         files: ['stylesheets/*.scss'],
-        tasks: 'shell:sass-development reload'
+        tasks: ['shell:sass-development', 'livereload']
       }
     },
-    'static-server': {
+    connect: {
+      livereload: {
+        options: {
+          port: 35729,
+          middleware: function(connect, options) {
+            return [lrSnippet, folderMount(connect, '.')]
+          }
+        }
+      },
       development: {
-        port: 8000,
-        base: '.',
-        runIndefinitely: false
+        options: {
+          port: 8000,
+          base: '.'
+        }
       },
       test: {
-        port: 8001,
-        base: '.',
-        runIndefinitely: false
+        options: {
+          port: 8001,
+          base: '.'
+        }
       },
       production: {
-        port: 8002,
-        base: './dist'
-      }
-    },
-    reload: {
-      port: 35729, // LR default
-      liveReload: {},
-      proxy: {
-        port: 8000
+        options: {
+          port: 8002,
+          base: './dist',
+          keepalive: true
+        }
       }
     }
   });
 
   // Default task
-  grunt.registerTask('default', 'lint static-server:development static-server:test shell:sass-development reload shell:jasmine shell:zombie watch');
-  grunt.registerTask('dist', 'shell:clean-build shell:clean-dist shell:sass-production requirejs hash shell:update_dist finalize-html');
-  grunt.registerTask('test', 'static-server:test shell:jasmine shell:zombie');
+  grunt.registerTask('test', ['connect:test', 'shell:jasmine', 'shell:zombie']);
+  grunt.registerTask('dist', ['shell:clean-build', 'shell:clean-dist', 'shell:sass-production', 'requirejs', 'hash', 'shell:update_dist', 'finalize-html']);
+  grunt.registerTask('default', ['jshint', 'livereload-start', 'connect:development', 'shell:sass-development', 'test', 'regarde']);
 
 };
